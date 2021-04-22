9f3c77c4975998f5ab6f8b743dbdb3e88d571a05
Eli Cusic <tntitans31@gmail.com>
2021-04-22 10:35:19 -0500

    set up index in decks controller with card data

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,8 @@
 class DecksController < ApplicationController
+
+    def index
+        @decks = Deck.all
+        render json: @decks.map{ |deck| deck.with_cards }
+    end
+
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,14 @@
 class Deck < ApplicationRecord
     has_many :cards
+
+    def with_cards
+        deck = {
+            id: self.id,
+            name: self.name,
+            cards: []
+        }
+        self.cards.each{ |card| deck[:cards] << {side_a: card.side_a, side_b: card.side_b} }
+        deck
+    end
+
 end
