b47408c9abc58ff2196a60ae65e72967f5c36da0
Eli Cusic <tntitans31@gmail.com>
2021-05-12 14:06:09 -0500

    set up cards index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
   root 'application#home'
-  
+
   resources :decks do 
     resources :cards
   end
+
+  resources :cards
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
